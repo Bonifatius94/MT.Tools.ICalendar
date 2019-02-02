f127d1ece18342fc31350d2176b126292c08750d
Marco Tröster <marco-troester@live.de>
2019-02-02 15:32:29 +0100

    Add project files.

---
+++ MT.Tools.Icalendar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MT.Tools.Icalendar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
