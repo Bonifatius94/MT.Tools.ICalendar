760ec8c57f6f11f3736b4f24f4fc0f73b92bea00
Marco Tröster <marco-troester@live.de>
2019-04-17 03:34:12 +0200

    - implemented duration and period of time

--- MT.Tools.ICalendar/DataObjects/PropertyValue/Time/DurationValue.cs
+++ MT.Tools.ICalendar/DataObjects/PropertyValue/Time/DurationValue.cs
@@ -29,19 +29,79 @@ namespace MT.Tools.ICalendar.DataObjects.PropertyValue.Time
         {
             // remove heading and trailing white spaces
             content = content.Trim();
-
+            
             // remove '+' sign
             content = content.Replace("+", "");
 
             // check if '-' is set
+            bool isNegative = content.Contains('-');
+            content = content.Replace("-", "");
+            content = content.Replace("P", "");
 
+            TimeSpan newDuration;
 
-            throw new NotImplementedException();
+            if (content.Contains('W'))
+            {
+                string contentAsString = content.Substring(0, content.IndexOf('W'));
+                int weeks = int.Parse(contentAsString);
+                newDuration = new TimeSpan(7 * weeks, 0, 0, 0);
+            }
+            else
+            {
+                int days = 0;
+                int hours = 0;
+                int minutes = 0;
+                int seconds = 0;
+
+                if (content.Contains('D'))
+                {
+                    string contentAsString = content.Substring(0, content.IndexOf('D'));
+                    days = int.Parse(contentAsString);
+                    content = content.Substring(content.IndexOf('D') + 1, content.Length - contentAsString.Length - 1);
+                }
+
+                if (content.Contains('H'))
+                {
+                    string contentAsString = content.Substring(0, content.IndexOf('H'));
+                    hours = int.Parse(contentAsString);
+                    content = content.Substring(content.IndexOf('H') + 1, content.Length - contentAsString.Length - 1);
+                }
+
+                if (content.Contains('M'))
+                {
+                    string contentAsString = content.Substring(0, content.IndexOf('M'));
+                    minutes = int.Parse(contentAsString);
+                    content = content.Substring(content.IndexOf('M') + 1, content.Length - contentAsString.Length - 1);
+                }
+
+                if (content.Contains('S'))
+                {
+                    string contentAsString = content.Substring(0, content.IndexOf('S'));
+                    seconds = int.Parse(contentAsString);
+                }
+
+                newDuration = new TimeSpan(days, hours, minutes, seconds);
+            }
+
+            newDuration = isNegative ? newDuration.Negate() : newDuration;
+            Duration = newDuration;
         }
 
         public string Serialize()
         {
-            throw new NotImplementedException();
+            bool isNegative = Duration < TimeSpan.Zero;
+            string content = $"{ (isNegative ? "-" : "") }P";
+            
+            if (Duration.Days % 7 == 0 && Duration.Hours == 0 && Duration.Minutes == 0 && Duration.Seconds == 0)
+            {
+                content += $"{ Duration.Days / 7 }W";
+            }
+            else
+            {
+                content += $"{ Duration.TotalDays }D{ Duration.Hours }H{ Duration.Minutes }M{ Duration.Seconds }S";
+            }
+
+            return content;
         }
 
         #endregion Methods
--- MT.Tools.ICalendar/DataObjects/PropertyValue/Time/PeriodOfTimeValue.cs
+++ MT.Tools.ICalendar/DataObjects/PropertyValue/Time/PeriodOfTimeValue.cs
@@ -1,25 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MT.Tools.ICalendar.DataObjects.PropertyValue.Time
 {
+    public enum PeriodOfTimeValueType
+    {
+        Duration,
+        FixEnd
+    }
+
     public class PeriodOfTimeValue : IPropertyValueImpl
     {
         #region Constructor
 
+        public PeriodOfTimeValue() { }
+
         public PeriodOfTimeValue(DateTime start, DateTime end)
         {
-            Start = start;
-            End = end;
+            _start = new DateTimeValue(start);
+            _end = new DateTimeValue(end);
+        }
+
+        public PeriodOfTimeValue(DateTime start, TimeSpan duration)
+        {
+            _start = new DateTimeValue(start);
+            _end = new DateTimeValue(start + duration);
         }
 
         #endregion Constructor
 
         #region Members
 
-        public DateTime Start { get; private set; }
-        public DateTime End { get; private set; }
+        private PeriodOfTimeValueType _type;
+
+        private DateTimeValue _start;
+        private DateTimeValue _end;
+        private DurationValue _duration;
+
+        public DateTime Start { get { return _start.DateTime; } }
+        public DateTime End { get { return (_type == PeriodOfTimeValueType.Duration) ? (_start.DateTime + _duration.Duration) : _end.DateTime; } }
+        public TimeSpan Duration { get { return (_type == PeriodOfTimeValueType.Duration) ? _duration.Duration : (_end.DateTime - _start.DateTime); ; } }
 
         #endregion Members
 
@@ -27,14 +49,42 @@ namespace MT.Tools.ICalendar.DataObjects.PropertyValue.Time
 
         public void Deserialize(string content)
         {
-            // TODO: implement logic
-            throw new NotImplementedException();
+            // remove heading and trailing white spaces
+            content = content.Trim();
+
+            // retrieve the two parts of the period
+            var parts = content.Split('/', StringSplitOptions.RemoveEmptyEntries);
+
+            // make sure there are exactly two parts
+            if (parts?.Count() != 2) { throw new ArgumentException($"Invalid period of time content ({ content }) detected!"); }
+
+            // deserialize start
+            _start = new DateTimeValue();
+            _start.Deserialize(parts[0]);
+
+            // determine the type of the second part
+            _type = parts[1].Contains('P') ? PeriodOfTimeValueType.Duration : PeriodOfTimeValueType.FixEnd;
+
+            if (_type == PeriodOfTimeValueType.Duration)
+            {
+                // deserialize duration
+                _duration = new DurationValue();
+                _duration.Deserialize(parts[1]);
+            }
+            else
+            {
+                // deserialize end
+                _end = new DateTimeValue();
+                _end.Deserialize(parts[1]);
+            }
         }
 
         public string Serialize()
         {
-            // TODO: implement logic
-            throw new NotImplementedException();
+            string start = new DateTimeValue(Start).Serialize();
+            string end = (_type == PeriodOfTimeValueType.Duration) ? new DurationValue(Duration).Serialize() : new DateTimeValue(End).Serialize();
+
+            return $"{ start }/{ end }";
         }
 
         #endregion Methods
